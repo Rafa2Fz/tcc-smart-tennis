3e7e4e67bea5fe227948193959ae50abb0f68d93
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-23 23:10:06 -0300

    Corrigindo configuração ESLINT

--- backend/src/shared/http/routes/index.ts
+++ backend/src/shared/http/routes/index.ts
@@ -1,18 +1,17 @@
-import { Router } from "express";
-import reservasRotas from "modules/reservas/routes/reservas.routes";
-import sessionRotas from "modules/usuario/routes/session.routes";
-import usuarioRotas from "modules/usuario/routes/usuario.routes";
-import folgaRotas from "modules/folga/routes/folga.routes";
-import { ensureAuthenticated } from "../middleware/ensureAuthenticate";
-import pagamentoRotas from "modules/pagamento/routes/pagamento.routes";
+import { Router } from 'express';
+import reservasRotas from 'modules/reservas/routes/reservas.routes';
+import sessionRotas from 'modules/usuario/routes/session.routes';
+import usuarioRotas from 'modules/usuario/routes/usuario.routes';
+import folgaRotas from 'modules/folga/routes/folga.routes';
+import pagamentoRotas from 'modules/pagamento/routes/pagamento.routes';
+import { ensureAuthenticated } from '../middleware/ensureAuthenticate';
 
-const rotas = Router()
+const rotas = Router();
 
-rotas.use('/usuario', usuarioRotas)
-rotas.use('/auth', sessionRotas)
-rotas.use('/reservas', ensureAuthenticated, reservasRotas)
-rotas.use('/folga', ensureAuthenticated, folgaRotas)
-rotas.use('/pagamento',  pagamentoRotas)
+rotas.use('/usuario', usuarioRotas);
+rotas.use('/auth', sessionRotas);
+rotas.use('/reservas', ensureAuthenticated, reservasRotas);
+rotas.use('/folga', ensureAuthenticated, folgaRotas);
+rotas.use('/pagamento', pagamentoRotas);
 
-
-export default rotas
+export default rotas;
--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -1,42 +1,39 @@
-import "reflect-metadata";
-import 'config/database'
-import 'shared/container'
-import 'express-async-errors'
+import 'reflect-metadata';
+import 'config/database';
+import 'shared/container';
+import 'express-async-errors';
 
 import dotenv from 'dotenv';
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
+import { AppError } from 'shared/error/AppError';
+import cors from 'cors';
 import rotas from './routes';
-import { AppError } from "shared/error/AppError";
-import path from 'path'
-import cors from 'cors'
-
 
 dotenv.config({
-    path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
-    override: true
-})
+  path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
+  override: true,
+});
 
+const app = express();
 
-const app = express()
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.use(rotas)
+app.use(rotas);
 
 app.use((err, request: Request, response: Response, next) => {
-    try {
-        if (err instanceof AppError) {
-            response.status(err.statusCode).json(err.message)
-        } else {
-            response.status(500).json({ message: 'Internal server error' })
-            console.log(err.message)
-        }
-    } catch (err) {
-        next(err);
+  try {
+    if (err instanceof AppError) {
+      response.status(err.statusCode).json(err.message);
+    } else {
+      response.status(500).json({ message: 'Internal server error' });
+      console.log(err.message);
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.listen(`${process.env.PORT || 3003}`, () => {
-    console.log(`Server iniciado na porta ${process.env.PORT? process.env.PORT:3003}`)
-})
+  console.log(`Server iniciado na porta ${process.env.PORT ? process.env.PORT : 3003}`);
+});
