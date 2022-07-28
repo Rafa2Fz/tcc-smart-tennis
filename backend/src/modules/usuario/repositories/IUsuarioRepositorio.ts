7c476d3c62c014111dace00ae726f2a003e66618
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-28 14:54:16 -0300

    Conclusão dos testes para a criação do usuário

--- backend/src/modules/usuario/repositories/IUsuarioRepositorio.ts
+++ backend/src/modules/usuario/repositories/IUsuarioRepositorio.ts
@@ -1,8 +1,7 @@
-import { Usuario } from "entity/Usuario";
+import { Usuario } from 'entity/Usuario';
 
 export interface IUsuarioRepositorio {
-    criar(usuario: Pick<Usuario, "email" | "nome" | "password" | "tipoUsuarioId">): Promise<Usuario>
-    salvar(usuario: Usuario): Promise<Usuario>
+    criar(usuario: Pick<Usuario, 'email' | 'nome' | 'password' | 'tipoUsuarioId'>): Promise<Usuario>
     buscarPorEmail(email: string): Promise<Usuario | undefined>
     buscarPorId(id: string): Promise<Usuario | undefined>
     atualizarCreditoUsuario(usuario: Usuario): Promise<Usuario>
--- backend/src/modules/usuario/useCase/CriarUsuario/CriarUsuario.spec.ts
+++ backend/src/modules/usuario/useCase/CriarUsuario/CriarUsuario.spec.ts
@@ -1,20 +1,41 @@
+import { Usuario } from 'entity/Usuario';
 import FakeUsersRepository from 'modules/usuario/repositories/Fake/FakeUsersRepository';
-// import { HashProvider } from 'shared/container/providers/HashProvider/HashProvider';
-// import { CriarUsuarioUseCase } from './CriarUsuarioUseCase';
+import { HashProvider } from 'shared/container/providers/HashProvider/HashProvider';
+import { AppError } from 'shared/error/AppError';
+import { CriarUsuarioUseCase } from './CriarUsuarioUseCase';
 
-// let criarUsuario: CriarUsuarioUseCase;
+let criarUsuario: CriarUsuarioUseCase;
 let fakeUsersRepository: FakeUsersRepository;
 
 describe('Create a new user', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
-    // const hashProvider = new HashProvider();
-    // criarUsuario = new CriarUsuarioUseCase(fakeUsersRepository, hashProvider);
+    const hashProvider = new HashProvider();
+    criarUsuario = new CriarUsuarioUseCase(fakeUsersRepository, hashProvider);
   });
+
   it('should be able to create a new user', async () => {
-    const user = await fakeUsersRepository.criar({
-      nome: 'Rafael', email: 'test@gmail.com', password: '12354Abcd', tipoUsuarioId: 1,
+    const user = await criarUsuario.executar({
+      nome: 'Rafael', email: 'test@gmail.com', password: '12354Abcd',
     });
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create to user with two same emails', async () => {
+    await criarUsuario.executar({
+      nome: 'Rafael',
+      email: 'teste@gmail.com',
+      password: '12354Abcd',
+    });
+
+    try {
+      await criarUsuario.executar({
+        nome: 'Rafael',
+        email: 'teste@gmail.com',
+        password: '12354Abcd',
+      });
+    } catch (e) {
+      expect(e).toBeInstanceOf(AppError);
+    }
   });
 });
