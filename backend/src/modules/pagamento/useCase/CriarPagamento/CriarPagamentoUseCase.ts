4c216826db4f7693773462199ac47489627cab87
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-30 01:09:29 -0300

    Fix issues

--- backend/src/modules/pagamento/useCase/CriarPagamento/CriarPagamentoUseCase.ts
+++ backend/src/modules/pagamento/useCase/CriarPagamento/CriarPagamentoUseCase.ts
@@ -1,8 +1,8 @@
-import { inject, injectable } from "tsyringe";
-import mercadoPago from 'mercadopago'
-import { AppError } from "shared/error/AppError";
-import { IUsuarioRepositorio } from "modules/usuario/repositories/IUsuarioRepositorio";
-import { IPagamentoRepositorio } from "../../repositories/IPagamentoRepositorio";
+import { inject, injectable } from 'tsyringe';
+import mercadoPago from 'mercadopago';
+import { AppError } from 'shared/error/AppError';
+import { IUsuarioRepositorio } from 'modules/usuario/repositories/IUsuarioRepositorio';
+import { IPagamentoRepositorio } from '../../repositories/IPagamentoRepositorio';
 
 interface IRequest {
     usuarioId: string
@@ -11,75 +11,66 @@ interface IRequest {
 
 interface IResponse {
     id: string;
-} 
+}
 @injectable()
 export class CriarPagamentoUseCase {
-
-    constructor(
+  constructor(
         @inject('UsuarioRepositorio')
         private usuarioRepositorio: IUsuarioRepositorio,
         @inject('PagamentoRepositorio')
-        private pagamentoRepositorio: IPagamentoRepositorio
-    ){}
+        private pagamentoRepositorio: IPagamentoRepositorio,
+  ) {}
 
-    async execute({usuarioId, pacote}: IRequest): Promise<IResponse> {
-        const usuario = await this.usuarioRepositorio.buscarPorId(usuarioId)
- 
-        const produtos = {
-            pacote100: ()=> {
-                return 100
-            },
-            pacote30: ()=>{
-                return 30 
-            }
-        }
-        let gerarTotal = produtos[pacote];
+  async execute({ usuarioId, pacote }: IRequest): Promise<IResponse> {
+    const usuario = await this.usuarioRepositorio.buscarPorId(usuarioId);
 
-    
-        let total = gerarTotal()
-   
+    const produtos = {
+      pacote100: () => 100,
+      pacote30: () => 30,
+    };
+    const gerarTotal = produtos[pacote];
 
-        if(typeof total !== 'number'){
-            throw new AppError('Pacote inválido', 400)
-        }
+    const total = gerarTotal();
 
-        mercadoPago.configure({
-            access_token: process.env.PROD_ACCESS_TOKEN
-        })
-
-        let preference = {
-         
-            items: [
-                {
-                  title: pacote,
-                  unit_price: total,
-                  quantity: 1,
-                }
-              ],
-              back_urls: {
-                success: "https://smart-tennis-front.herokuapp.com/dashboard",
-                failure: "https://smart-tennis-front.herokuapp.com/dashboard",
-                pending: "https://smart-tennis-front.herokuapp.com/dashboard",
-            },
-    
-          };
-
-       try {
-        const response =  await mercadoPago.preferences.create(preference)
-        
-        
-        let {id} = response.body
-
-        const pagamento = await this.pagamentoRepositorio.criar(usuario, id, total)
-        
-        await this.pagamentoRepositorio.salvar(pagamento)
-        
-        usuario.setCredito = usuario.getCredito + total
-        await this.usuarioRepositorio.salvar(usuario)
-        return {id}
-       } catch(err){
-        console.log(err)
-       }
+    if (typeof total !== 'number') {
+      throw new AppError('Pacote inválido', 400);
     }
-}
 
+    mercadoPago.configure({
+      access_token: process.env.NODE_ENV === 'dev' ? process.env.DEV_ACCESS_TOKEN_MERCADOPAGO : process.env.PROD_ACCESS_TOKEN,
+    });
+
+    const preference = {
+
+      items: [
+        {
+          title: pacote,
+          unit_price: total,
+          quantity: 1,
+        },
+      ],
+      back_urls: {
+        success: 'https://smart-tennis-front.herokuapp.com/dashboard',
+        failure: 'https://smart-tennis-front.herokuapp.com/dashboard',
+        pending: 'https://smart-tennis-front.herokuapp.com/dashboard',
+      },
+
+    };
+
+    try {
+      const response = await mercadoPago.preferences.create(preference);
+
+      const { id } = response.body;
+
+      const pagamento = await this.pagamentoRepositorio.criar(usuario, id, total);
+
+      await this.pagamentoRepositorio.salvar(pagamento);
+
+      usuario.setCredito = usuario.getCredito + total;
+      await this.usuarioRepositorio.salvar(usuario);
+      return { id };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
