a560869933e0162c8a7827e58909cb56fb5cfded
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-30 00:30:16 -0300

    Issue fixed

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  url: `${process.env.NODE_ENV === 'dev' ? process.env.LOCAL_URL : process.env.DATABASE_URL}`,
+  url: process.env.NODE_ENV === 'dev' ? process.env.LOCAL_URL : process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   extra: process.env.NODE_ENV === 'dev' ? {} : {
