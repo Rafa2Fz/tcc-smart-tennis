345dc76d8e44ef590130496811d457250f9e0b25
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-07-30 00:27:40 -0300

    Issue fixed

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -3,11 +3,11 @@ module.exports = {
   url: `${process.env.NODE_ENV === 'dev' ? process.env.LOCAL_URL : process.env.DATABASE_URL}`,
   synchronize: false,
   logging: false,
-  extra: `${process.env.NODE_ENV === 'dev' ? {} : {
+  extra: process.env.NODE_ENV === 'dev' ? {} : {
     ssl: {
       rejectUnauthorized: false,
     },
-  }}`,
+  },
   entities: [
     'src/entity/**/*{.ts,.js}',
   ],
