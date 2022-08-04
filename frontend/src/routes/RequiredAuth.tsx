651eca238c35d02c38746744c591c8bcc9f386e3
Rafa2Fz <rafaelfrohlich@hotmail.com>
2022-08-03 23:45:58 -0300

    Fixed some issues

--- frontend/src/routes/RequiredAuth.tsx
+++ frontend/src/routes/RequiredAuth.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useUsuario } from "../hooks/user";
 interface IRequiredAuth {
@@ -7,8 +8,11 @@ interface IRequiredAuth {
 
 function RequireAuth({ isPrivate = false, children }: IRequiredAuth) {
   let auth = useUsuario();
+  const { atualizarUsuario } = useUsuario();
   let location = useLocation();
-
+  useEffect(() => {
+    atualizarUsuario();
+  }, [location, atualizarUsuario]);
   return isPrivate === !!auth.user ? (
     children
   ) : (
